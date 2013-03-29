d6c1fa1d8adf4b3a3e4c7dcf7c89bc43110d5240
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2013-03-29 08:17:52 +0100

    Refactorization

--- src/main/scala/impl/analyser/ViewAnalyser.scala
+++ src/main/scala/impl/analyser/ViewAnalyser.scala
@@ -8,43 +8,45 @@ import impl.data.{EntitiesTypes, XY}
 class ViewAnalyser(view: String) {
 
   val viewSize = math.sqrt(view.length).toInt
-
   val viewDistance = (viewSize - 1) / 2
 
+  var goodPlants = List[XY]()
+  var goodBeasts = List[XY]()
+  var badPlants = List[XY]()
+  var badBeasts = List[XY]()
+  var enemyBots = List[XY]()
+  var enemyMiniBots = List[XY]()
+  var myBots = List[XY]()
+  var myMiniBots = List[XY]()
+
+  //init lists
+  findEntities()
+
+
   def getViewPointFromRelative(xy: XY): Char = view.charAt((xy.y + viewDistance) * viewSize + (xy.x + viewDistance))
-
   def getViewPoint(x: Int, y: Int): Char = view.charAt(y * viewSize + x)
-
   def getViewPointRelative(x: Int, y: Int): Char = view.charAt((y + viewDistance) * viewSize + (x + viewDistance))
-
-  lazy val goodPlants = findEntitiesOfType(EntitiesTypes.GOOD_PLANT)
-
-  lazy val goodBeasts = findEntitiesOfType(EntitiesTypes.GOOD_BEAST)
-
-  lazy val badPlants = findEntitiesOfType(EntitiesTypes.BAD_PLANT)
-
-  lazy val badBeasts = findEntitiesOfType(EntitiesTypes.BAD_BEAST)
-
-  lazy val enemyBots = findEntitiesOfType(EntitiesTypes.ENEMY_BOT)
-
-  lazy val enemyMiniBots = findEntitiesOfType(EntitiesTypes.ENEMY_MINI_BOT)
-
-  lazy val myBots = findEntitiesOfType(EntitiesTypes.MY_BOT)
-
-  lazy val myMiniBots = findEntitiesOfType(EntitiesTypes.MY_MINI_BOT)
-
   def enemiesCount = badPlants.size + enemyBots.size + enemyMiniBots.size
 
-  private def findEntitiesOfType(entityType: Char): List[XY] = {
-
-    var foundEntities = List[XY]()
+  private def findEntities() {
     for (y <- 0 until viewSize) {
       for (x <- 0 until viewSize) {
-        if (getViewPoint(x, y) == entityType) {
-          foundEntities ::= new XY(x - viewDistance, y - viewDistance)
-        }
+        addEntityToProperList(getViewPoint(x, y), new XY(x - viewDistance, y - viewDistance))
       }
     }
-    foundEntities
   }
+
+  private def addEntityToProperList(viewPoint: Char, xyLocation:XY) {
+    viewPoint match {
+      case EntitiesTypes.GOOD_PLANT => goodPlants ::= xyLocation
+      case EntitiesTypes.BAD_PLANT => badPlants ::= xyLocation
+      case EntitiesTypes.BAD_BEAST => badBeasts ::= xyLocation
+      case EntitiesTypes.ENEMY_BOT => enemyBots ::= xyLocation
+      case EntitiesTypes.ENEMY_MINI_BOT => enemyMiniBots ::= xyLocation
+      case EntitiesTypes.MY_BOT => myBots ::= xyLocation
+      case EntitiesTypes.MY_MINI_BOT => myMiniBots ::= xyLocation
+      case _ => {}
+    }
+  }
+
 }
